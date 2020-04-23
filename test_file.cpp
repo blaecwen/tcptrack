d1cb5576f0e51ab57d8a32f6237ba19a43a06b7d
Blaecwen <blaecwen@gmail.com>
2020-04-23 19:58:54 +0300

    Second fix

--- test_file.cpp
+++ test_file.cpp
@@ -1,11 +1,13 @@
  
          for (int i = 0; i < primaryFileNames.size(); i++)
          {
-            if (!addCamera(videoLayout,
-                    httpPort,
-                    cameraOptions,
-                    {primaryFileNames[i]},
-                    {secondaryFileNames[i]}))
+            if (!addCamera(
+                videoLayout,
+                httpPort,
+                cameraOptions,
+                {primaryFileNames[i]},
+                {secondaryFileNames[i]}))
+
              {
                  return false;
              }
